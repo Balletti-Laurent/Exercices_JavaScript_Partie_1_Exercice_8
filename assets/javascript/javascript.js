53df6d7d6b6ebb96e2c48411ad5d635754d47a2d
Balletti-Laurent <balletti.laurent@gmail.com>
2018-10-24 10:42:09 +0200

    Exercice 8

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,15 @@
+function show(){
+  var age= document.getElementById('age').value;
+  if (isNaN(age)==true && age !=0){
+    alert('Erreur, ceci n\'est pas un nombre');
+  }
+  else {
+    if
+    age>= 18;
+    alert('Vous êtes majeur');
+    else {
+      age< 18;
+    }
+    alert('Vous êtes mineur');
+}
+}
